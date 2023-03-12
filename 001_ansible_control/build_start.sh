d78d30503b45202eb0e4ea350ea211ecf8454afe
kytay <kytay@users.noreply.github.com>
2023-03-12 15:54:56 +0800

    chore: update build_run.sh

--- 001_ansible_control/build_start.sh
+++ 001_ansible_control/build_start.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 # Build
-docker build -t ${IMAGE} .
+docker build \
+--build-arg "ARG_TZ=Asia/Singapore" \
+--build-arg "ARG_CONTAINER_USER=ansible" \
+-t ${IMAGE} .
 
 # Start
 mkdir -p ./../projects && \
-docker run -it \
+docker run -it --rm \
+--name ansible-runtime \
 -v $(pwd)/../projects:$(docker image inspect -f '{{.Config.WorkingDir}}' ${IMAGE}) \
 ${IMAGE}
